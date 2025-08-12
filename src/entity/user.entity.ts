81edf3cf400f55fa5ef3960f49d7ba692bb9580f
azambembem <azamjan980713@gmail.com>
2025-08-12 21:40:54 +0900

    entity swagger

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -11,18 +12,23 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ description: 'name', example: '홍길동' })
   @Column({ unique: true })
   name: string;
 
+  @ApiProperty({ description: 'email', example: 'Hc5yj@example.com' })
   @Column()
   email: string;
 
+  @ApiProperty({ description: 'password', example: '1234' })
   @Column()
   password: string;
 
+  @ApiProperty({ description: 'update date', example: 'update-date' })
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @ApiProperty({ description: 'create date', example: 'date' })
   @CreateDateColumn()
   createdAt: Date;
 }
