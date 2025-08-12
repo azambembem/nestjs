e60fa2c5dcf78a1a07632287c50c6e25015b3393
azambembem <azamjan980713@gmail.com>
2025-08-12 18:15:08 +0900

    register step 2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { log } from 'console';
 
 @Controller('auth')
 export class AuthController {
   constructor(protected authService: AuthService) {}
 
   @Post('register')
-  register(@Body() body: { name: string; email: string; password: string }) {
-    return {
-      message: 'User registered successfully',
-      data: body,
-    };
+  async register(
+    @Body() body: { name: string; email: string; password: string },
+  ) {
+    return this.authService.register(body.name, body.email, body.password);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async hashPassword(password: string): Promise<string> {
+    console.log(password);
+    const saltRounds = 10;
+    return await bcrypt.hash(password, saltRounds);
+  }
+
+  async register(name: string, email: string, password: string) {
+    const hashedPassword = await this.hashPassword(password);
+    return {
+      name,
+      email,
+      password: hashedPassword,
+    };
+  }
+}
