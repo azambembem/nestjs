9e3ddad3e7a68cef6c7e1928de6ff921b1242efe
azambembem <azamjan980713@gmail.com>
2025-08-12 16:16:48 +0900

    auth create

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,11 @@
-import { Controller, Get, Ip, Query } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 
-@Controller()
+@Controller('auth')
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly configService: ConfigService,
   ) {}
-
-  @Get()
-  getHello(@Ip() ip: string): string {
-    console.log(this.configService.get<string>('ENVIRONMENT'));
-    return this.appService.getHello();
-  }
-
-  @Get('name')
-  getName(@Query('name') name: string): string {
-    return ` ${name} hello `;
-  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 import ConfigModule from './config';
 
 @Module({
-  imports: [ConfigModule(), BoardModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule(), BoardModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
